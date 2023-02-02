b48ce89484395592407d94e0d8e8591e24b27dc1
Karishma Nadar <karishkutty4@gmail.com>
2023-02-03 00:55:44 +0530

    Ui

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+class CustomColors {
+  static Color hotelname = const Color.fromARGB(255, 42, 35, 80);
+}
--- lib/Widgets/hotel_detail_card.dart
+++ lib/Widgets/hotel_detail_card.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:tripdetail/Constants/colors.dart';
 
 class DetailsCard extends StatelessWidget {
   const DetailsCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
-      child: DetailsCard(),
+    return Card(
+      child: Row(children: [
+        // Image widget
+        Column(
+          children: [
+            RatingBar.builder(
+              initialRating: 3,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: CustomColors.hotelname,
+              ),
+              onRatingUpdate: (rating) {
+                print(rating);
+              },
+            ),
+            Text(
+              "Hotle name",
+              style: TextStyle(fontSize: 18, color: CustomColors.hotelname),
+            )
+          ],
+        )
+      ]),
     );
   }
 }
